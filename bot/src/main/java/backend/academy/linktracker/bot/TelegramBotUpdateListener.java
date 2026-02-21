60392b09ffe3890281a49a7d7ecf49b6af156bc1
vadomerka <pradomiri@gmail.com>
2026-02-21 21:38:52 +0300

    fix: refactoring

--- bot/src/main/java/backend/academy/linktracker/bot/TelegramBotUpdateListener.java
+++ bot/src/main/java/backend/academy/linktracker/bot/TelegramBotUpdateListener.java
@@ -1,12 +1,10 @@
 package backend.academy.linktracker.bot;
 
-import backend.academy.linktracker.bot.properties.TelegramProperties;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -14,11 +12,8 @@ import java.util.List;
 public class TelegramBotUpdateListener implements UpdatesListener {
     private final TelegramBot bot;
 
-    private final TelegramProperties properties;
-
-    public TelegramBotUpdateListener(TelegramBot bot, TelegramProperties properties) {
+    public TelegramBotUpdateListener(TelegramBot bot) {
         this.bot = bot;
-        this.properties = properties;
         bot.setUpdatesListener(this);
     }
 
@@ -40,33 +35,25 @@ public class TelegramBotUpdateListener implements UpdatesListener {
         Long chatId = update.message().chat().id();
 
         if ("/start".equalsIgnoreCase(text)) {
-            String message = "start message";
-
-            SendMessage request = new SendMessage(chatId, message)
-                .parseMode(ParseMode.HTML);
-            SendResponse sendResponse = bot.execute(request);
+            sendMessage(chatId, "Hello world!");
             return;
         }
 
         if ("/help".equalsIgnoreCase(text)) {
-//            telegramBot.send(token, new SendMessageRequest(chatId,
-//                """
-//                Available commands:
-///start - Welcome message
-///help - List all commands
-///echo <text> - Repeat what you say
-//                """));
+            String message = """
+                Available commands:
+                /start - Welcome message
+                /help - List all commands
+                """;
+            sendMessage(chatId, message);
             return;
         }
 
-        if (text.startsWith("/echo")) {
-//            String echo = text.replaceFirst("/echo", "").trim();
-//            telegramBot.sendMessage(token, new SendMessageRequest(chatId,
-//                echo.isEmpty() ? "You didn’t provide any text." : echo));
-            return;
-        }
+        sendMessage(chatId, "Неизвестная команда. Воспользуйтесь /help, чтобы посмотреть список доступных команд.");
+    }
 
-//        telegramBot.sendMessage(token, new SendMessageRequest(chatId,
-//            "Unknown command. Type /help for a list of available commands."));
+    private void sendMessage(long chatId, String message) {
+        SendMessage request = new SendMessage(chatId, message).parseMode(ParseMode.HTML);
+        bot.execute(request);
     }
 }
