3347be0c7764a4b6b4f992a74f3308c2cdbc13a4
vadomerka <pradomiri@gmail.com>
2026-06-18 18:39:55 +0300

    fix: refactoring

---
+++ scrapper/src/main/resources/db/002-outbox-message.sql
@@ -0,0 +1,12 @@
+-- liquibase formatted sql
+
+-- changeset author:003-1
+CREATE TABLE IF NOT EXISTS outbox_message (
+    id UUID PRIMARY KEY NOT NULL,
+    topic VARCHAR(255) NOT NULL,
+    message_key VARCHAR(255),
+    payload TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    error_message TEXT
+);
