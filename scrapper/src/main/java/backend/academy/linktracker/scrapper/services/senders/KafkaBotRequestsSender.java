5a3aa7bcba7536aee44372503f70131fa2709dd7
vadomerka <pradomiri@gmail.com>
2026-06-18 20:45:08 +0300

    fix: refactoring

--- scrapper/src/main/java/backend/academy/linktracker/scrapper/services/senders/KafkaBotRequestsSender.java
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/services/senders/KafkaBotRequestsSender.java
@@ -3,16 +3,11 @@ package backend.academy.linktracker.scrapper.services.senders;
 import backend.academy.linktracker.models.exceptions.KafkaSenderException;
 import backend.academy.linktracker.models.http.internal.LinkUpdateRequest;
 import backend.academy.linktracker.scrapper.properties.KafkaSenderProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class KafkaBotRequestsSender implements BotRequestsSender {
-    private static final Logger log = LoggerFactory.getLogger(KafkaBotRequestsSender.class);
-
     private final KafkaSenderProperties properties;
     private final KafkaSender kafkaSender;
 
@@ -22,7 +17,6 @@ public class KafkaBotRequestsSender implements BotRequestsSender {
     }
 
     public void sendUpdates(LinkUpdateRequest request) {
-        SendResult<String, String> result;
         String topic = properties.getTopic(); // String.valueOf(request.chatId());
         String key = properties.getGroup();
         String message = new ObjectMapper().writeValueAsString(request);
