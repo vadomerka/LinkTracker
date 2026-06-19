285271ba6b010fdc4202e56bc6e7238e203b77c0
vadomerka <pradomiri@gmail.com>
2026-06-19 09:02:14 +0300

    fix: refactoring

--- bot/src/test/java/backend/academy/linktracker/bot/TestBotApplication.java
+++ bot/src/test/java/backend/academy/linktracker/bot/TestBotApplication.java
@@ -1,12 +1,12 @@
-package backend.academy.linktracker.bot;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestBotApplication {
-
-    static void main(String[] args) {
-        SpringApplication.from(BotApplication::main)
-                .with(TestcontainersConfiguration.class)
-                .run(args);
-    }
-}
+// package backend.academy.linktracker.bot;
+//
+// import org.springframework.boot.SpringApplication;
+//
+// public class TestBotApplication {
+//
+//    static void main(String[] args) {
+//        SpringApplication.from(BotApplication::main)
+//                .with(TestcontainersConfiguration.class)
+//                .run(args);
+//    }
+// }
--- scrapper/src/test/java/backend/academy/linktracker/scrapper/SourceControllerTest.java
+++ scrapper/src/test/java/backend/academy/linktracker/scrapper/SourceControllerTest.java
@@ -1,8 +1,6 @@
 package backend.academy.linktracker.scrapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
